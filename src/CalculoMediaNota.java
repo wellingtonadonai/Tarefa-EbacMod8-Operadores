30da7d64642849247ff0eef3ca8cea420100dd4a
wellington <wellington.h.s.oliveira@hotmail.com>
2024-07-09 12:18:24 -0300

    tarefa sobre operadores calculo de media

---
+++ src/CalculoMediaNota.java
@@ -0,0 +1,18 @@
+public class CalculoMediaNota {
+
+    public static void main(String[] args) {
+        mediaNotasAluno();
+    }
+
+    private static void mediaNotasAluno() {
+
+        int aluno1 = 8;
+        int aluno2 = 7;
+        int aluno3 = 9;
+        int aluno4 = 8;
+
+        int mediasnotas = (aluno1 + aluno2 + aluno3 + aluno4)/4;
+
+        System.out.println(mediasnotas);
+    }
+}
